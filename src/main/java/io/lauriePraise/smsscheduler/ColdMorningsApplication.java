f511c4e4a061c9a27a0b229da7705a628d0db889
Praise Uadiale <puadiale@gmail.com>
2022-05-08 00:14:06 +0100

    Enforcing Immutability with configuration classes

--- src/main/java/io/lauriePraise/smsscheduler/ColdMorningsApplication.java
+++ src/main/java/io/lauriePraise/smsscheduler/ColdMorningsApplication.java
@@ -1,9 +1,14 @@
 package io.lauriePraise.smsscheduler;
 
+import io.lauriePraise.smsscheduler.config.TwilioConfiguration;
+import javax.swing.event.TreeWillExpandListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableConfigurationProperties(value = {TwilioConfiguration.class})
 public class ColdMorningsApplication {
 
 	public static void main(String[] args) {
--- src/main/java/io/lauriePraise/smsscheduler/config/TwilioConfiguration.java
+++ src/main/java/io/lauriePraise/smsscheduler/config/TwilioConfiguration.java
@@ -4,20 +4,45 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.stereotype.Component;
 
 /** Created by Puadi on April, 2022 */
-@Configuration
 @ConfigurationProperties(prefix = "twilio")
-@Data
+@ConstructorBinding
 public class TwilioConfiguration {
 
-  private String accountSid;
-  private String authToken;
-  private String trialNumber;
+  private final String accountSid;
+  private final String authToken;
+  private final String trialNumber;
+
+
+  public TwilioConfiguration(String accountSid, String authToken, String trialNumber) {
+    this.accountSid = accountSid;
+    this.authToken = authToken;
+    this.trialNumber = trialNumber;
+  }
+
+  public String getAccountSid() {
+    return accountSid;
+  }
+
+  public String getAuthToken() {
+    return authToken;
+  }
+
+  public String getTrialNumber() {
+    return trialNumber;
+  }
 
   // JAVA POJO:
 }
+//@ConfigurationProperties is not going to register a spring bean
+// Adding @EnableConfigurationProperties(value = MyCustomProperties.class) will create a spring bean
